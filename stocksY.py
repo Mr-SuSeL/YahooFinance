f15d83661426ca9e4ee5da20600d24fda4985f57
Lukasz Suslowicz <susel.invest@gmail.com>
2023-11-09 15:03:50 +0100

    container applied

--- stocksY.py
+++ stocksY.py
@@ -4,6 +4,15 @@ import yfinance as yf
 window = tk.Tk()
 window.title("Stock info")
 
+# Kontener interfejsu
+topWidget = tk.Frame(window)
+label = tk.Label(topWidget, text="Stock ticker:")
+label.pack(side=tk.LEFT)
+entry = tk.Entry(topWidget)
+entry.pack(side=tk.RIGHT)
+topWidget.pack() # dodane na samej górze
+
+
 scrollbar = tk.Scrollbar(window)
 textBox = tk.Text(window, height=10, width=70, padx=5, pady=5, font="Helvetica 12")
 scrollbar.pack(side=tk.RIGHT, fill = tk.Y)
@@ -12,6 +21,7 @@ textBox.pack(expand=True, fill=tk.BOTH)
 scrollbar.config(command=textBox.yview)
 textBox.config(yscrollcommand=scrollbar.set)
 
+# Pobieramy dane z serwera za pomocą Yahoo Finance
 
 
 window.mainloop()
